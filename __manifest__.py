06305879416824c2fc3ee07e50a9ee4c1eeed75d
Janil A K <janil@enzapps.com>
2024-11-23 16:53:28 +0530

    Multi Currency Issue

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Invoice Partial Lable Fix',
+    'version': '16.0.1.1.0',
+    'category': 'Invoicing',
+    'summary': "Invoice Partial Lable Fix",
+    'author': 'Enzapps Private Limited',
+    'company': 'Enzapps Private Limited',
+    'website': 'http://www.enzapps.com',
+    'description': """
+    Invoice Partial Lable Fix
+""",
+    'depends': ['sale',
+                'account','sale_discount_total','purchase_discount','enz_fix_invoice_partial_label'
+                ],
+    'data': [
+    ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'application': True,
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+}
